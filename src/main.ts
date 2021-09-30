a7b2e3746862a187631747f82c66a43f8cd8f87b
jin <jinchul112@gmai.com>
2021-09-30 14:02:06 +0200

    up

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import session from 'express-session';
+import passport from 'passport';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3030;
+
+  app.use(cookieParser());
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: false,
+      secret: process.env.COOKIE_SECRET,
+      cookie: {
+        httpOnly: true,
+      },
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   await app.listen(port);
   console.log(`listening on port ${port}`);
