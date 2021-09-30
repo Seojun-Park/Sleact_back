a6694bcd6a802c00c0fc6367a24c8d65f4cce556
jin <jinchul112@gmai.com>
2021-09-30 13:33:55 +0200

    y

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,12 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('user')
-  getHello(): string {
-    return this.appService.getUser();
-  }
-
-  @Post('user')
-  postHello(): string {
-    return this.appService.postUser();
+  @Get()
+  getHello() {
+    return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3030;
 
-  await app.listen(3000);
+  await app.listen(port);
   console.log(`listening on port ${port}`);
 
   if (module.hot) {
